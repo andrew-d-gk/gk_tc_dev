66b886ec2c6b013d230647e6b5151a05f76e38cd
mailer <mail@mail.com>
2017-08-07 15:54:13 +0100

    updated nunit from 2.63 to 2.6.4

--- gk_teamcity_test.UnitTests/MockTest.cs
+++ gk_teamcity_test.UnitTests/MockTest.cs
@@ -1,10 +1,5 @@
 ﻿namespace Gk_teamcity_test.UnitTests
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
     using NUnit.Framework;
 
     /// <summary>
